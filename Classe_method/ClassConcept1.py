bad5b5fe2b7e85740adbf922ef95d19102687308
ksinha <ksinha@PU-NB-TEMP5.na.rtdom.net>
2018-12-20 14:22:43 +0530

    __init__ Constructor + Overriding Concepts

--- Classe_method/ClassConcept1.py
+++ Classe_method/ClassConcept1.py
@@ -25,5 +25,35 @@ priya.setAge(29)
 print(priya.getAge())
 priyanka=class2()
 priyanka.setAge("Twenty Nine")
-print(priyanka.getAge())           
+print(priyanka.getAge())
+
+#__init Constructor
+class class3(object):
+    def  __init__(self,a,b):
+        self.aa=a
+        self.bb=b
+x=class3(4,5)
+print("aa=",x.aa,"||bb=",x.bb)
+x.aa=44
+x.bb=55
+print("aa=",x.aa,"||bb=",x.bb)
+
+#Overriding the attribute
+class class4(object):
+    a=4
+y=class4()
+print(y.a)
+y.a=44
+print(y.a)
+del y.a
+print(y.a)
+#del y.a
+#print(y.a) # Gives error as no value is there in Attribute a "AttributeError: a"
+
+
+#Working with Class and Instance Data
+
+
+    
+       
         
