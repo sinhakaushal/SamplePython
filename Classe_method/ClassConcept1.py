b5eba8f19fcb98b29bebd43b90b4c547b57295f3
ksinha <ksinha@PU-NB-TEMP5.na.rtdom.net>
2018-12-20 13:16:42 +0530

    Added a new ClassConcept1.py for practising Class Concept

---
+++ Classe_method/ClassConcept1.py
@@ -0,0 +1,18 @@
+'''
+Created on Dec 20, 2018
+
+@author: ksinha
+'''
+class classconcept1():
+    var=9
+    def firstPrint(self):
+        print("Hello Print 1")
+        print(self)
+obj=classconcept1()
+print(obj)
+print(obj.var)
+obj.firstPrint()
+print(obj)
+
+        
+        
