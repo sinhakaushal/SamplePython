40972db5bdda74b3178ca504bba4e6de2a1223c6
ksinha <ksinha@PU-NB-TEMP5.na.rtdom.net>
2018-12-24 16:15:29 +0530

    updated

--- Email/mutliplemails.py
+++ Email/mutliplemails.py
@@ -5,27 +5,31 @@ Created on Dec 24, 2018
 '''
 import smtplib
 import csv
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from future.backports.email import encoders
+
 def sendmail(toaddr,msg):  
     # creates SMTP session 
     s = smtplib.SMTP('smtp.gmail.com', 587)
-      
-    # start TLS for security 
     s.starttls()
-    #https://myaccount.google.com/lesssecureapps  --- MAKE UR GMAIL ALLOW TO USE THIS SERVICE
-      
-    # Authentication 
     s.login("kaushal.opensource@gmail.com", "Accenture@123")  #(username,password)
-      
-    # message to be sent 
-    message =msg    # "Hello Test From Python2"
-      
-    # sending the mail 
-    s.sendmail("KAUSHAL OPENSOURCE", toaddr, message)
-    #( 
-      
-    # terminating the session 
-    s.quit()
+    #message =msg    # "Hello Test From Python2"
+    main_msg=MIMEMultipart()
+    main_msg.attach(MIMEText(msg,'plain'))
+    fname='TestPython.txt'
+    attch=open(r'C:\Users\ksinha\eclipse-workspace\SamplePython\Email\TestPython.txt','rb')
+#     p=MIMEBase('application','octet-stream')
+#     p.set_payload((attch).read())
 
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition', "attachment; filename= %s" % fname)
+    main_msg.attach(p)
+    txt=main_msg.as_string()
+    fromaddr="kaushal.opensource@gmail.com"
+    s.sendmail(fromaddr, toaddr, txt)
+    s.quit()
 
 with open(r'C:\Users\ksinha\eclipse-workspace\SamplePython\Email\contact.csv','r') as file:
     reader = csv.reader(file)
